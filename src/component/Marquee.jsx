6c0ddfc61bbc0794ea594216d0edd72884caae20
Syed zain ali shah <zaingit780@gmail.com>
2024-02-07 23:13:58 +0500

    add animation in marquee

--- src/component/Marquee.jsx
+++ src/component/Marquee.jsx
@@ -4,27 +4,21 @@ import React from "react";
 const Marquee = () => {
   return (
     <div className="w-full py-10 bg-[#004d43] rounded-tl-3xl rounded-tr-3xl">
-      <div className="text border-t border-b  border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden">
-        <motion.h1
-          initial={{ x: 0 }}
-          animate={{ x: -100 }}
-          transition={{ repeat: "Infinity", ease: "linear", duration: 5 }}
-          className="leading-none text-[25vw] uppercase font-founderGrotesk -mb-[2vw] -mt-[4vw] text-white"
-        >
-          we are ochi
-        </motion.h1>
-        <motion.h1
-          initial={{ x: 0 }}
-          animate={{ x: -100 }}
-          transition={{
-            repeat: "Infinity",
-            ease: "linear",
-            duration: 5,
-          }}
-          className="leading-none text-[25vw] uppercase font-founderGrotesk -mb-[2vw] -mt-[4vw] text-white"
-        >
-          we are ochi
-        </motion.h1>
+      <div className="text border-t border-b  border-zinc-300 flex whitespace-nowrap overflow-hidden">
+        {[1, 2].map((_, i) => (
+          <motion.h1
+            initial={{ x: "0" }}
+            animate={{ x: "-100%" }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 10,
+            }}
+            className="leading-none text-[25vw] uppercase font-founderGrotesk -mb-[2vw] -mt-[4vw] text-white pr-20"
+          >
+            we are ochi
+          </motion.h1>
+        ))}
       </div>
     </div>
   );
