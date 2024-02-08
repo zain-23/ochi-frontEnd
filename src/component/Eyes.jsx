c7573d7134dc41613df080bcc319047c7323ddc4
Syed zain ali shah <zaingit780@gmail.com>
2024-02-08 12:30:24 +0500

    create eyes effect

--- src/component/Eyes.jsx
+++ src/component/Eyes.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import img from "../assets/Top-Viewbbcbv-1-scaled.jpg";
 
 const Eyes = () => {
+  const [rotate, setRotate] = useState(0);
+  console.log(rotate);
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      const deltaX = mouseX - window.innerWidth / 2;
+      const deltaY = mouseY - window.innerHeight / 2;
+      let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle - 180);
+    });
+  }, []);
   return (
     <div className="w-full h-screen overflow-hidden">
       <div
@@ -10,14 +24,24 @@ const Eyes = () => {
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center gap-x-8">
           <div className="w-[15vw] h-[15vw] bg-white rounded-full flex justify-center items-center">
             <div className="w-2/3 h-2/3 bg-black rounded-full relative">
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[6] w-full">
+              <div
+                style={{
+                  transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                }}
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[6] w-full"
+              >
                 <div className="h-10 w-10 rounded-full bg-white"></div>
               </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] bg-white rounded-full flex justify-center items-center">
             <div className="w-2/3 h-2/3 bg-black rounded-full relative">
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[6] w-full">
+              <div
+                style={{
+                  transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                }}
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[6] w-full"
+              >
                 <div className="h-10 w-10 rounded-full bg-white"></div>
               </div>
             </div>
